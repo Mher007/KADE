26da37fda8e6e7b13fda7a06f1c3efbf29813795
Mher Grigoryan <mger_grigoryan@mail.ru>
2024-03-24 22:17:28 +0400

    first commit

---
+++ src/Script/script.js
@@ -0,0 +1,17 @@
+document.getElementById('contact-form').addEventListener ('submit', function(event));
+ {
+  event.preventDefault();
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+  // Код для обработки отправки формы
+  console.log('Имя: ' + name);
+  console.log('Email: ' + email);
+  console.log('Сообщение: ' + message);
+  alert('Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.');
+        document.getElementById('contact-form').reset();
+    };
+
+    // git remote add origin https://github.com/Mher007/-.git
+    // git branch -M main
+    // git push -u origin main

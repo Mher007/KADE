03bda72803665858c66e04002801383ca4234ee6
Mher Grigoryan <mger_grigoryan@mail.ru>
2024-03-25 21:40:21 +0400

    Added a call button

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.getElementById('contact-form').addEventListener ('submit', function(eve
   let email = document.getElementById('email').value;
   let message = document.getElementById('message').value;
   // Код для обработки отправки формы
-  console.log('Имя: ' + name);
-  console.log('Email: ' + email);
+  console.log('name' + name);
+  console.log('Email:' + email);
   console.log('Сообщение: ' + message);
   alert('Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.');
         document.getElementById('contact-form').reset();
